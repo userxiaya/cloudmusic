6a4044e9e2ac0a39d5d66f091ff1da685a41b549
v_jyhhuang <v_jyhhuang@tencent.com>
2018-12-17 21:08:41 +0800

    update

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.use('/apis', function(req, res) {
     var url = 'http://47.107.134.246:3000/' + req.url.replace('/apis','');
     req.pipe(request(url)).pipe(res);
 });
-app.use(express.static('dist'));
+app.use(express.static('./dist/'));
 var server = app.listen(8080, function () {
   var host = server.address().address
   var port = server.address().port
