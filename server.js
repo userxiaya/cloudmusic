0e9211a8f78c2eb6b88d7fbda3dce8940f36bc06
userxiaya <15361835871@163.com>
2019-01-08 20:46:20 +0800

    update

--- server.js
+++ server.js
@@ -1,14 +1,8 @@
 var express = require('express');
 var app = express();
-var request = require('request');
 var app = express();
-app.use('/apis', function(req, res) {
-    var url = 'http://47.107.134.246:3000' + req.url.replace('/apis/','');
-    console.log(url)
-    req.pipe(request(url)).pipe(res);
-});
 app.use(express.static('dist'));
-var server = app.listen(8080, function () {
+var server = app.listen(8079, function () {
   var host = server.address().address
   var port = server.address().port
   console.log("应用实例，访问地址为 http://%s:%s", host, port)
